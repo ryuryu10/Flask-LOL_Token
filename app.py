ba95bb7c7296baf29d0d53d903928860d11c5dda
RYUSS - ABCD <ryuryu10@naver.com>
2021-10-06 15:56:42 +0900

    [ UPDATE ] 필요없는 코드 삭제

--- app.py
+++ app.py
@@ -9,26 +9,6 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
-@app.route("/simple_chart")
-def chart():
-    legend = 'Monthly Data'
-    labels = ["January", "February", "March", "April", "May", "June", "July", "August"]
-    values = [10, 9, 8, 7, 6, 4, 7, 8]
-    return render_template('index.html', values=values, labels=labels, legend=legend)
-
-
-@app.route("/line_chart")
-def line_chart():
-    legend = 'Temperatures'
-    temperatures = [73.7, 73.4, 73.8, 72.8, 68.7, 65.2,
-                    61.8, 58.7, 58.2, 58.3, 60.5, 65.7,
-                    70.2, 71.4, 71.2, 70.9, 71.3, 71.1]
-    times = ['12:00PM', '12:10PM', '12:20PM', '12:30PM', '12:40PM', '12:50PM',
-             '1:00PM', '1:10PM', '1:20PM', '1:30PM', '1:40PM', '1:50PM',
-             '2:00PM', '2:10PM', '2:20PM', '2:30PM', '2:40PM', '2:50PM']
-    return render_template('line_chart.html', values=temperatures, labels=times, legend=legend)
-
-
 @app.route("/time_chart")
 def time_chart():
     legend = 'Temperatures'
