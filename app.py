b78d023b3cf2dba9b74af452185bb673c28f8fbf
RYUSS - ABCD <ryuryu10@naver.com>
2021-10-07 11:06:30 +0900

    build2

--- app.py
+++ app.py
@@ -4,43 +4,28 @@ from datetime import time
 import glob
 import csv
 
+from flask.helpers import total_seconds
+
 app = Flask(__name__)
 
 @app.route("/")
 def main():
+    total_token_data = []
+    date_data = []
     for csv_files in [file for file in glob.glob("./data/*") if file.endswith(".csv")]:
         opener = open(csv_files, 'r', encoding='utf-8')
         reader = csv.reader(opener)
         for line in reader:
-            print(line)
+            total_token_data.append(line[2])
+            date_data.append(line[0]+' '+line[1])
         opener.close()
-    return render_template('index.html')
-
-@app.route("/time_chart")
-def time_chart():
-    legend = 'Temperatures'
-    temperatures = [73.7, 73.4, 73.8, 72.8, 68.7, 65.2,
-                    61.8, 58.7, 58.2, 58.3, 60.5, 65.7,
-                    70.2, 71.4, 71.2, 70.9, 71.3, 71.1]
-    times = [time(hour=11, minute=14, second=15),
-             time(hour=11, minute=14, second=30),
-             time(hour=11, minute=14, second=45),
-             time(hour=11, minute=15, second=00),
-             time(hour=11, minute=15, second=15),
-             time(hour=11, minute=15, second=30),
-             time(hour=11, minute=15, second=45),
-             time(hour=11, minute=16, second=00),
-             time(hour=11, minute=16, second=15),
-             time(hour=11, minute=16, second=30),
-             time(hour=11, minute=16, second=45),
-             time(hour=11, minute=17, second=00),
-             time(hour=11, minute=17, second=15),
-             time(hour=11, minute=17, second=30),
-             time(hour=11, minute=17, second=45),
-             time(hour=11, minute=18, second=00),
-             time(hour=11, minute=18, second=15),
-             time(hour=11, minute=18, second=30)]
-    return render_template('index.html', values=temperatures, labels=times, legend=legend)
+    return render_template(
+        'index.html', 
+        values=total_token_data, 
+        labels=date_data, 
+        legend='패스토큰',
+        current_token=total_token_data[-1]
+        )
 
 
 if __name__ == "__main__":
