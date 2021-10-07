1e15b40746d19c2d4b47ff8c8d588e112497ae91
RYUSS - ABCD <ryuryu10@naver.com>
2021-10-07 12:46:50 +0900

    build5

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import render_template
 from datetime import datetime, time
 import glob
 import csv
+import time
 
 from flask.helpers import total_seconds
 
@@ -19,12 +20,14 @@ def main():
             total_token_data.append(line[2])
             date_data.append(line[0]+' '+line[1])
         opener.close()
-    print(date_data[-1].split(" ")[0]+" "+date_data[-1].split(" ")[1])
-    AHSH = datetime.strptime(date_data[-1].split(" ")[0]+" "+date_data[-1].split(" ")[1],"%Y/%m/%d %H:%M:%S")
-    BHSH = datetime.strptime(date_data[-5].split(" ")[0]+" "+date_data[-5].split(" ")[1],"%Y/%m/%d %H:%M:%S")
+    AHSH = datetime.strptime(date_data[-1].split(" ")[0]+" "+date_data[-1].split(" ")[1],"%Y/%m/%d %H:%M:%S") #<---
+    BHSH = datetime.strptime(date_data[-5].split(" ")[0]+" "+date_data[-5].split(" ")[1],"%Y/%m/%d %H:%M:%S") #<---
     END_DATE = datetime.strptime("2021-11-9 6:00:00","%Y-%m-%d %H:%M:%S")
-    NOW_TIME = datetime.now()
-    remaining_hash = int((END_DATE-NOW_TIME).seconds)
+    now = time.localtime()
+    NOW = "%04d-%02d-%02d %02d:%02d:%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
+    NOW = datetime.strptime(NOW,"%Y-%m-%d %H:%M:%S")
+    remaining_hash = (int((END_DATE - NOW).days) * 86400) / 720
+    print(END_DATE, NOW)
     xvc = (int(total_token_data[-1]) - int(total_token_data[-5])) / int(((AHSH- BHSH).seconds))
     return render_template(
         'index.html', 
