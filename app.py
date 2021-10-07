5e89d3264401534424c3c5b34da4643eb8def398
RYUSS - ABCD <ryuryu10@naver.com>
2021-10-07 11:59:33 +0900

    build3

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import render_template
-from datetime import time
+from datetime import datetime, time
 import glob
 import csv
 
@@ -19,12 +19,19 @@ def main():
             total_token_data.append(line[2])
             date_data.append(line[0]+' '+line[1])
         opener.close()
+    print(date_data[-1].split(" ")[0]+" "+date_data[-1].split(" ")[1])
+    AHSH = datetime.strptime(date_data[-1].split(" ")[0]+" "+date_data[-1].split(" ")[1],"%Y/%m/%d %H:%M:%S")
+    BHSH = datetime.strptime(date_data[-5].split(" ")[0]+" "+date_data[-5].split(" ")[1],"%Y/%m/%d %H:%M:%S")
+    print(int(total_token_data[-1]), int(total_token_data[-5]), AHSH, BHSH, int(((AHSH- BHSH).seconds)))
+    xvc = (int(total_token_data[-1]) - int(total_token_data[-5])) / int(((AHSH- BHSH).seconds))
+    print(xvc * int(((AHSH- BHSH).seconds)))
     return render_template(
         'index.html', 
         values=total_token_data, 
         labels=date_data, 
         legend='패스토큰',
-        current_token=total_token_data[-1]
+        current_token=total_token_data[-1],
+        hash=xvc * int(((AHSH- BHSH).seconds))
         )
 
 
